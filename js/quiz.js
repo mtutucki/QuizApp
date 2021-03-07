9ab9d0935be6c3f5399f1b1d5e16ea1f571bc04d
Marcin Tutucki <m.tutucki@gmail.com>
2021-03-07 20:34:26 +0100

    Add function checking if answer is true.
    If answer is true then nextBtn shows up. Then user click it and go to
    next question. When end game user can reset progress and start again

--- js/quiz.js
+++ js/quiz.js
@@ -2,8 +2,9 @@ let $question;
 let $anwersButtons;
 let $firstAnswerBtn;
 let $secondAnsweBtn;
-let $counter = 0;
+let $counter =4;
 let $checkBtn;
+let $nextBtn;
 let $points = 0;
 
 
@@ -13,11 +14,13 @@ const prepareDOMElements = () => {
     $secondAnsweBtn = document.body.querySelector(".answerSecond");
     $checkBtn = document.body.querySelector(".checkBtn");
     $anwersButtons = document.body.querySelector(".anwers");
+    $nextBtn = document.body.querySelector(".nextBtn");
 }
 
 const prepareDOMEvents = () => {
     $anwersButtons.addEventListener("click", whichButtonWasClicked);
     $checkBtn.addEventListener("click", checkAnswer);
+    $nextBtn.addEventListener("click", startGame);
 }
 
 const startGame = () => {
@@ -36,14 +39,18 @@ const startGame = () => {
 
     const questions = [firstQuiz,secondQuiz,thirdQuiz,fourthQuiz,fifthQuiz];
 
-    for (let i=0; i<=$counter; i++){
-        resetButtons();
-        $question.innerHTML = questions[i].question;
-        $firstAnswerBtn.innerHTML = questions[i].firstAnswer;
-        $secondAnsweBtn.innerHTML = questions[i].secondAnswer;
-        if ($counter == questions.length) {
-            endGame();
+    if ($counter<questions.length)
+    {
+        for (let i=0; i<=$counter; i++){
+            resetButtons();
+            $question.innerHTML = questions[i].question;
+            $firstAnswerBtn.innerHTML = questions[i].firstAnswer;
+            $secondAnsweBtn.innerHTML = questions[i].secondAnswer;
         }
+    }
+    
+    else if ($counter >= questions.length) {
+        endGame();
     }
 }
 
@@ -51,16 +58,28 @@ const endGame = () => {
             $question.innerHTML = `Twój wynik to: ${$points} / 5`;
             $firstAnswerBtn.style.display = "none";
             $secondAnsweBtn.style.display = "none";
-            $checkBtn.style.display = "none";
+            endGameButton();
 }
 
+const endGameButton = () => {
+        $nextBtn.innerHTML = "Spróbuj jeszcze raz";
+        $counter = 0;
+        $points = 0;
+}
 
 
 const resetButtons = () => {
     $firstAnswerBtn.classList.remove("choosedBtn");
     $firstAnswerBtn.classList.add("btn");
+    $firstAnswerBtn.style.backgroundColor = "";
+    $firstAnswerBtn.style.display = "block";
     $secondAnsweBtn.classList.remove("choosedBtn");
     $secondAnsweBtn.classList.add("btn");
+    $secondAnsweBtn.style.backgroundColor = "";
+    $secondAnsweBtn.style.display = "block";
+    $nextBtn.style.display = "none";
+    $checkBtn.style.display = "block";
+    $nextBtn.innerHTML = "dalej";
 }
 
 
@@ -83,13 +102,20 @@ const checkAnswer = () => {
     if ($firstAnswerBtn.classList.contains("choosedBtn")){
         $counter++;
         $points++;
+        $firstAnswerBtn.style.backgroundColor = "rgba(11, 175, 60, 0.815)";
+        showNextQuestion();
     }
     else if ($secondAnsweBtn.classList.contains("choosedBtn")){
         $counter++;
+        $secondAnsweBtn.style.backgroundColor = "rgba(201, 18, 18, 0.582)";
+        showNextQuestion();
     }
 }
 
-
+const showNextQuestion = () => {
+    $checkBtn.style.display = "none";
+    $nextBtn.style.display = "block";
+}
 
 const main = () => {
     prepareDOMElements();
