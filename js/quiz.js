db6253acac3098287750b1920b53719acb6f5129
Marcin Tutucki <m.tutucki@gmail.com>
2021-03-08 21:47:38 +0100

    Resolve problem with shuffle answers in mobile view
    Add flex directon column for mobile and row for rest devices

--- js/quiz.js
+++ js/quiz.js
@@ -1,3 +1,4 @@
+// declaring global variables
 let $question;
 let $anwersButtons;
 let $firstAnswerBtn;
@@ -9,6 +10,7 @@ let $points = 0;
 const $goodAnswerColor = "rgba(11, 175, 60, 0.815)";
 const $badAnswerColor = "rgba(201, 18, 18, 0.582)";
 
+// declaring global variables
 const prepareDOMElements = () => {
     $question = document.body.querySelector(".question"); 
     $firstAnswerBtn = document.body.querySelector(".answerFirst");
@@ -18,12 +20,109 @@ const prepareDOMElements = () => {
     $nextBtn = document.body.querySelector(".nextBtn");
 }
 
+// events for quiz
 const prepareDOMEvents = () => {
     $anwersButtons.addEventListener("click", whichButtonWasClicked);
     $checkBtn.addEventListener("click", checkAnswer);
     $nextBtn.addEventListener("click", startGame);
 }
 
+// check which button user choose
+// if user choose one then button get style for active button
+// when user try another button then remove active style from previous
+const whichButtonWasClicked = (e) => {   
+    if (e.target.closest("button").classList.contains("answerFirst") ){
+            e.target.closest("button").classList.toggle("choosedBtn");
+            e.target.closest("button").classList.toggle("btn");
+            $secondAnsweBtn.classList.remove("choosedBtn");
+            $secondAnsweBtn.classList.add("btn");
+        }
+        else if (e.target.closest("button").classList.contains("answerSecond") ){
+            e.target.closest("button").classList.toggle("choosedBtn");
+            e.target.closest("button").classList.toggle("btn");
+            $firstAnswerBtn.classList.remove("choosedBtn");
+            $firstAnswerBtn.classList.add("btn");
+        }
+}
+
+// set check answer button invisible and analogously for next question button
+const showNextQuestion = () => {
+    $checkBtn.style.display = "none";
+    $nextBtn.style.display = "block";
+}
+
+// this function send to check answer button listener
+// if user choose god answer then button style = good answer = green
+// if user choose bad answer then button style = bad answer = red
+// then call out showNextQuestion function
+const checkAnswer = (e) => {
+    if ($firstAnswerBtn.classList.contains("choosedBtn")){
+        $counter++;
+        $points++;
+        $firstAnswerBtn.style.backgroundColor = $goodAnswerColor;
+        showNextQuestion();
+    }
+    else if ($secondAnsweBtn.classList.contains("choosedBtn")){
+        $counter++;
+        $secondAnsweBtn.style.backgroundColor = $badAnswerColor;
+        showNextQuestion();
+    }
+}
+
+// function for shuffle answers buttons. 
+// create 2 variable and init then with random numbers from 1 to 2
+// when variables are same then flex order is set to -1 
+// this causes that second button sometimes set as first 
+const shuffleAnswer = () => {
+    const min = 0;
+    const max = 3;
+    const firstRandom = (Math.floor(Math.random() * (max - min)) + min);
+    const secondRandom = (Math.floor(Math.random() * (max - min)) + min);
+    const secondAnswer = document.body.querySelector(".answerSecond");
+
+    if (firstRandom == secondRandom){
+        secondAnswer.style.order = "-1"
+    }
+    else{
+        secondAnswer.style.order = "0";
+    }
+}
+
+// when user click next question then are of styles has to be reset to default
+const resetButtons = () => {
+    $firstAnswerBtn.classList.remove("choosedBtn");
+    $firstAnswerBtn.classList.add("btn");
+    $firstAnswerBtn.style.backgroundColor = "";
+    $firstAnswerBtn.style.display = "block";
+    $secondAnsweBtn.classList.remove("choosedBtn");
+    $secondAnsweBtn.classList.add("btn");
+    $secondAnsweBtn.style.backgroundColor = "";
+    $secondAnsweBtn.style.display = "block";
+    $nextBtn.style.display = "none";
+    $checkBtn.style.display = "block";
+    $nextBtn.innerHTML = "dalej";
+}
+
+// set text inside button to "try again" and reset counter and points
+const endGameButton = () => {
+    $nextBtn.innerHTML = "Spróbuj jeszcze raz";
+    $counter = 0;
+    $points = 0;
+}
+
+// show earned points and hide buttons except "try again"
+const endGame = () => {
+    $question.innerHTML = `Twój wynik to: ${$points} / 5`;
+    $firstAnswerBtn.style.display = "none";
+    $secondAnsweBtn.style.display = "none";
+    endGameButton();
+}
+
+// main function of app
+// here all of questions are created, then they are convert to objects
+// question array stores Quiz objects
+// if counter is less than questions lengts then next question show up
+// else it show endGame function
 const startGame = () => {
     class Quiz {
         constructor (question, firstAnswer, secondAnswer){
@@ -55,97 +154,6 @@ const startGame = () => {
         endGame();
     }
 }
-
-const shuffleAnswer = () => {
-    const min = 0;
-    const max = 3;
-    const firstRandom = (Math.floor(Math.random() * (max - min)) + min);
-    const secondRandom = (Math.floor(Math.random() * (max - min)) + min);
-    const secondAnswer = document.body.querySelector(".answerSecond");
-
-    if (firstRandom == secondRandom){
-        secondAnswer.style.order = "-1"
-    }
-    else{
-        secondAnswer.style.order = "0";
-    }
-
-
-}
-
-
-const endGame = () => {
-            $question.innerHTML = `Twój wynik to: ${$points} / 5`;
-            $firstAnswerBtn.style.display = "none";
-            $secondAnsweBtn.style.display = "none";
-            endGameButton();
-}
-
-const endGameButton = () => {
-        $nextBtn.innerHTML = "Spróbuj jeszcze raz";
-        $counter = 0;
-        $points = 0;
-}
-
-const resetButtons = () => {
-    $firstAnswerBtn.classList.remove("choosedBtn");
-    $firstAnswerBtn.classList.add("btn");
-    $firstAnswerBtn.style.backgroundColor = "";
-    $firstAnswerBtn.style.display = "block";
-    $secondAnsweBtn.classList.remove("choosedBtn");
-    $secondAnsweBtn.classList.add("btn");
-    $secondAnsweBtn.style.backgroundColor = "";
-    $secondAnsweBtn.style.display = "block";
-    $nextBtn.style.display = "none";
-    $checkBtn.style.display = "block";
-    $nextBtn.innerHTML = "dalej";
-}
-
-
-const whichButtonWasClicked = (e) => {
-
-        
-    if (e.target.closest("button").classList.contains("answerFirst") ){
-            e.target.closest("button").classList.toggle("choosedBtn");
-            e.target.closest("button").classList.toggle("btn");
-            $secondAnsweBtn.classList.remove("choosedBtn");
-            $secondAnsweBtn.classList.add("btn");
-        }
-        else if (e.target.closest("button").classList.contains("answerSecond") ){
-            e.target.closest("button").classList.toggle("choosedBtn");
-            e.target.closest("button").classList.toggle("btn");
-            $firstAnswerBtn.classList.remove("choosedBtn");
-            $firstAnswerBtn.classList.add("btn");
-        }
-
-}
-
-const checkAnswer = (e) => {
-
-
-    if ($firstAnswerBtn.classList.contains("choosedBtn")){
-        $counter++;
-        $points++;
-        $firstAnswerBtn.style.backgroundColor = $goodAnswerColor;
-        showNextQuestion();
-    }
-    else if ($secondAnsweBtn.classList.contains("choosedBtn")){
-        $counter++;
-        $secondAnsweBtn.style.backgroundColor = $badAnswerColor;
-        showNextQuestion();
-    }
-
-
-
-
-}
-
-const showNextQuestion = () => {
-    $checkBtn.style.display = "none";
-    $nextBtn.style.display = "block";
-}
-
-
 
 const main = () => {
     prepareDOMElements();
